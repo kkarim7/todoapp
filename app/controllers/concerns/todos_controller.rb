2c702ad3107c207c8665462c080ad7f2d9f96818
Khaleel Karim <khaleel.karim@gmail.com>
2018-09-17 00:21:05 +0000

    add edit action

--- app/controllers/concerns/todos_controller.rb
+++ app/controllers/concerns/todos_controller.rb
@@ -1,40 +1,41 @@
 class TodosController < ApplicationController
   
   def new
-    
     @todo = Todo.new
-    
   end
   
   def create
-    
     @todo = Todo.new(todo_params)
-    
    if @todo.save
-      
       flash.notice = "Todo was created Successfully!"
       redirect_to todo_path(@todo)
-     
    else
-     
      render 'new'
-     
    end
-    
   end
   
   def show
-    
     @todo = Todo.find(params[:id])
-    
+  end
+  
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash.notice = "Todo was Successfully Updated!"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
   end
   
   private
   
   def todo_params
-    
     params.require(:todo).permit(:name, :description)
-    
   end
   
 end
